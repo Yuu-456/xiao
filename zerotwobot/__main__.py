15af8fd2a80042e6ecc6aa9618e6c7501d4c6827
Izumi Tachibana <99785450+Yuu-456@users.noreply.github.com>
2022-07-31 12:00:33 +0530

    Update __main__.py

--- zerotwobot/__main__.py
+++ zerotwobot/__main__.py
@@ -75,18 +75,27 @@ def get_readable_time(seconds: int) -> str:
 buttons = [
     [
         InlineKeyboardButton(
-            text="Add Me",
+            text="【Aᴅᴅ Miyamura Tᴏ Yᴏᴜʀ GC】",
             url=f"https://t.me/{dispatcher.bot.username}?startgroup=True",
         ),
     ],
     [
         InlineKeyboardButton(
-            " •Support  ",
+            " • Support • ",
             "https://t.me/itadori_bot_support"),
         InlineKeyboardButton(
-            " •Network ",
+            " • Network •",
             "https://t.me/frostxNetwork"),
-
+        InlineKeyboardButton(
+             " • Updates • ",
+             "https://t.me/izumixupdates",
+        ),
+    ],
+    [
+        InlineKeyboardButton(
+              text="【HELP】",
+              callback_data=f"help_back",    
+        ), 
     ]
 ] 
 
